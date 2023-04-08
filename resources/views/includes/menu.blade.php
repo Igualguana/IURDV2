d26b7bd8bb006335b63fbe6b940fdcab27fcb7f7
Randy Igualguana <randyigualguana@gmail.com>
2023-04-08 02:46:21 -0400

    se actualizo proyecto

---
+++ resources/views/includes/menu.blade.php
@@ -0,0 +1,13 @@
+<div class="panel panel-primary">
+    <div class="panel-heading">Menu</div>
+
+    <div class="panel-body">
+
+        <ul class="nav bav-pills nav-satcked"></ul>
+
+
+
+    </div>
+
+
+</div>    
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,27 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\MembersController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::resource("/student", StudentController::class);
 Route::resource("/member", MembersController::class);
-Route::get('/ver', [MembersController::class, 'ver'])->name('ver');
+
 Route::get('/members/inde', [MembersController::class, 'inde'])->name('inde');
+
+Auth::routes();
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth']], function() {
+  
+    Route::resource('users', UserController::class);
+    Route::resource('roles', RoleController::class);
+    Route::get('/ver', [MembersController::class, 'ver'])->name('ver');
+});
+
